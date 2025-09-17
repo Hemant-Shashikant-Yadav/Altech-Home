86f553693e496fd14edf7debe08c701fc8b4ef6b
Hemant Yadav <hemantsryadav@gmail.com>
2025-09-17 23:03:27 +0530

    Refactor CollaborationSection1 to improve heading structure and maintain consistent styling

--- src/components/CollaborationSection1.jsx
+++ src/components/CollaborationSection1.jsx
@@ -2,35 +2,35 @@ const CollaborationSection1 = () => {
   return (
     <div className="bg-white overflow-hidden">
       {/* Desktop Container */}
-      <div 
+      <div
         className="hidden lg:flex w-[90rem] h-[43.875rem]  mx-auto items-center justify-center"
         style={{ marginTop: "5.63rem" }}
       >
-        <h1 
+        <h1
           className="text-[6.528rem] font-semibold leading-normal"
-          style={{ 
-            color: "#121212", 
+          style={{
             fontFamily: "Montserrat",
             fontWeight: "600"
           }}
         >
-          Collaborating to accelerate progress, inspire change, and lead the
-          future.
+          <span style={{ color: "#5B5B5B" }}>Collaborating to </span>
+          <span style={{ color: "#000" }}>accelerate progress, inspire change, </span>
+          <span style={{ color: "#5B5B5B" }}>and lead the future.</span>
         </h1>
       </div>
 
       {/* Mobile Container */}
       <div className="lg:hidden px-[1rem] py-[3rem] mt-[3.5rem] sm:mt-[3rem] md:mt-[4rem]">
-        <h1 
+        <h1
           className="text-[2rem] sm:text-[2.5rem] md:text-[3.5rem] font-semibold leading-tight"
-          style={{ 
-            color: "#121212", 
+          style={{
             fontFamily: "Montserrat",
             fontWeight: "600"
           }}
         >
-          Collaborating to accelerate progress, inspire change, and lead the
-          future.
+          <span style={{ color: "#5B5B5B" }}>Collaborating to </span>
+          <span style={{ color: "#000" }}>accelerate progress, inspire change, </span>
+          <span style={{ color: "#5B5B5B" }}>and lead the future.</span>
         </h1>
       </div>
     </div>
