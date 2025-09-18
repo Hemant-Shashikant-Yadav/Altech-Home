c79bac51a32f9f868846651bcfc905de34295363
Atharv Sankpal <atharvsankpal799@gmail.com>
2025-09-18 23:58:21 +0530

    .

--- src/pages/CollaborationPage.jsx
+++ src/pages/CollaborationPage.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import CollaborationForm from '../components/CollaborationForm';
 import Footer from '../components/Footer';
 import Container from '../components/ui/Container';
 
 const CollaborationPage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <Container>
             <CollaborationForm />
